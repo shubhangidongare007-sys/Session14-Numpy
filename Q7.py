eb1606d571ca42f93487bd9697f4480e516bede1
shubhangidongare007-sys <shubhangidongare007@gmail.com>
2026-07-04 09:22:14 -0700

    Add files via upload

---
+++ Q7.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a=np.array([10,20,30,40])
+b=np.array([1,2,3,4])
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+
+print("Power:",a**b)
+
+print("Modulo:",a%b)
